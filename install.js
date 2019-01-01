a19b67176edcbe1a793cc11af05256ff40440786
semakov_andrey <semakov.andrey.web@yandex.ru>
2019-01-01 20:46:43 +0400

    commit

--- install.js
+++ install.js
@@ -5,27 +5,17 @@ const fs                        = require('fs');
 const path                      = require('path');
 const ncp                       = require('ncp');
 
-if(path.basename(path.resolve(__dirname, '../')) !== 'node_modules') return;
+if (path.basename(path.resolve(__dirname, '../')) !== 'node_modules') return;
 
 const templateJSON              = require('./package.json');
 const packageJSON               = require(`${project}package.json`);
-
-/* update configuration */
-delete templateJSON.dependencies['ncp'];
-const json = {
-  ...packageJSON,
-  dependencies: {
-    ...packageJSON.dependencies,
-    ...templateJSON.dependencies
-  }
-};
-fs.writeFile(path.resolve(__dirname, project, 'package.json'), JSON.stringify(json, null, 2), 'utf8', error => error ? console.error('\x1b[31m%s\x1b[0m', `Error: ${error}`) : console.log('Success: configuration updated'));
+const DEV                       = packageJSON && (packageJSON.name === 'sa-template-1' || packageJSON.name === 'sa-template-2');
 
 /* update source */
-if(!fs.existsSync(path.resolve(__dirname, project, 'src'))) {
+if (!fs.existsSync(path.resolve(__dirname, project, 'src'))) {
   ncp.ncp(path.resolve(__dirname, 'src'), path.resolve(__dirname, project, 'src'), error => {
-    if(!error) {
-      if(packageJSON.devDependencies && packageJSON.devDependencies['sa-template-2']) {
+    if (!error) {
+      if (packageJSON.devDependencies && packageJSON.devDependencies['sa-template-2']) {
         fs.unlinkSync(path.resolve(__dirname, project, 'src/scripts/main.js'));
         fs.unlinkSync(path.resolve(__dirname, project, 'src/modules/layout/layout.pug'));
         fs.unlinkSync(path.resolve(__dirname, project, 'src/modules/images/images.pug'));
@@ -44,3 +34,15 @@ if(!fs.existsSync(path.resolve(__dirname, project, 'src'))) {
   });
 }
 
+if (DEV) return;
+
+/* update configuration */
+delete templateJSON.dependencies['ncp'];
+const json = {
+  ...packageJSON,
+  dependencies: {
+    ...packageJSON.dependencies,
+    ...templateJSON.dependencies
+  }
+};
+fs.writeFile(path.resolve(__dirname, project, 'package.json'), JSON.stringify(json, null, 2), 'utf8', error => error ? console.error('\x1b[31m%s\x1b[0m', `Error: ${error}`) : console.log('Success: configuration updated'));
