47e1fa3b7fb499b8456839f4f3f56d84e507daa8
Семаков Андрей <asemakov@htc-cs.ru>
2019-01-16 15:42:25 +0400

    commit

--- src/modules/icon/icon.js
+++ src/modules/icon/icon.js
@@ -2,13 +2,14 @@
 
 export default class Icon {
   constructor() {
-    let icons = document.getElementById('svg-sprite');
+    const icons = document.getElementById('svg-sprite');
     if (icons) {
-      let url = icons.dataset.path,
-        ajax = new XMLHttpRequest();
+      const url = icons.dataset.path;
+
+      const ajax = new XMLHttpRequest();
       ajax.open('GET', url, true);
       ajax.send();
-      ajax.onload = e => icons.innerHTML = ajax.responseText;
+      ajax.onload = () => icons.innerHTML = ajax.responseText;
     }
   }
 }
