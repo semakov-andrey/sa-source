183778900b21ea822b0e9034432c907da222ac72
Семаков Андрей <asemakov@htc-cs.ru>
2019-03-13 10:43:18 +0400

    css custom properties

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,16 @@
 'use strict';
 
 import '@babel/polyfill';
+import cssVars          from 'css-vars-ponyfill';
+import detection        from 'sa-polyfills/build/sa-detection';
 import Icon             from '../modules/icon/icon';
 import Preloader        from '../modules/preloader/preloader';
 import Burger           from '../modules/burger/burger';
 import Modal            from '../modules/modal/modal';
+
+if (detection.isIE10Plus()) {
+  cssVars({ onlyVars: true });
+}
 
 new Icon();
 new Preloader();
